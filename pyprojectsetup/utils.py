a4b9f9d5967ad4d894eb03e6ff3e3e063ad8516d
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-03-03 23:38:17 +0100

    add to utils

--- pyprojectsetup/utils.py
+++ pyprojectsetup/utils.py
@@ -1,5 +1,13 @@
 import sys
 import shutil
+from collections.abc import Callable
+from typing import Any
+from pathlib import Path
+
+from InquirerPy import inquirer
+from InquirerPy.prompts import ListPrompt
+from virtualenv.discovery.py_spec import PythonSpec
+from virtualenv.discovery.windows import propose_interpreters
 
 
 def is_virtual_environment():
@@ -10,5 +18,48 @@ def is_windows():
     return sys.platform == "win32"
 
 
-def has_git_installed():
+def is_git_installed():
     return shutil.which("git") is not None
+
+
+def list_selection(
+    message: str,
+    choices: list[str],
+    long_instruction: str | None = None,
+    filter_index: int | None = None,
+    filter_func: Callable[[str], Any] | None = None,
+) -> ListPrompt:
+    kwargs = {
+        "message": message,
+        "choices": choices,
+    }
+    if long_instruction is not None:
+        kwargs["long_instruction"] = long_instruction
+    if filter_index is not None:
+        if filter_func is not None:
+            raise ValueError("cannot provide both filter index and filter function")
+        if not 0 <= filter_index < len(choices):
+            raise ValueError(f"invalid index {filter_index}, must be between 0 and {len(choices) - 1} inclusive")
+        if len(set(choices)) != len(choices):
+            raise ValueError("cannot filter list with duplicated choices")
+        kwargs["filter"] = lambda res: True if res == choices[filter_index] else False
+    if filter_func is not None:
+        kwargs["filter"] = filter_func
+    return inquirer.select(**kwargs)
+
+
+def get_python_versions() -> dict[tuple[int, int], Path]:
+    found: dict[tuple[int, int], Path] = {}
+    versions = ((3, 8), (3, 9), (3, 10), (3, 11), (3, 12))
+    # TODO: filter anaconda, windows store versions
+    for v in versions:
+        interpreter = next(propose_interpreters(PythonSpec.from_string_spec(f"{v[0]}.{v[1]}-64"), None, None), None)
+        if interpreter is not None:
+            executable = Path(interpreter.executable)
+            found[v] = executable
+        else:
+            interpreter = next(propose_interpreters(PythonSpec.from_string_spec(f"{v[0]}.{v[1]}-32"), None, None), None)
+            if interpreter is not None:
+                executable = Path(interpreter.executable)
+                found[v] = executable
+    return found
